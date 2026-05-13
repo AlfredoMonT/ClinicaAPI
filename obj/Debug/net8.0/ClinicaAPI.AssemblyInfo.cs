c9bb685ceb00892f8334ecd88dae13f8c1d958ec
AlfredoMonT <020101610F@uandina.edu.pe>
2026-05-13 14:52:17 -0500

    Ejercicio Propuesto 01: Extensión de modelo y persistencia

--- obj/Debug/net8.0/ClinicaAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/ClinicaAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30c734b466268910843455122b850f24de4107c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
